768ddcfae0be73e4da8d67cdb258961b577cec49
Abdullah <abdullahmz4640@gmail.com>
2026-01-12 03:05:42 +0500

    Add JS calculator logic

--- app.js
+++ app.js
@@ -9,11 +9,40 @@ for (let i = 0; i < button.length; i++) {
         let value = btn.textContent;
         console.log (value);
         display.value += value;
-        if(value==="c"){
+
+        if(value === "c"){
             display.value= "";
         }
-    })
+        calculation();
 
+    })
     
+}
+
+
+const calculation = () => {
+
+    let arr = display.value.split('');
+    console.log (arr);
+
+    let currentValue= "";
+    let initailValue = [];
+    let finalValue =[];
+    let opretor = "" ;
+
+    for(let i=0; i<=arr.length-1; i++){
+        
+        let num = arr[i];
+
+        if(num >= '0' && num <= '9'){
+            currentValue += num ;
+            console.log (currentValue);
+
+        } else if(num ==='+' || num ==='-' || num ==='*' || num ==='/'){
+           
+             initailValue = currentValue.map(number);
+             console.log(initailValue);
+        }
+    } 
     
 }

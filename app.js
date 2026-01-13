2e3d7f40e6c4fbc41cdc42c7abc19b7b30da1adb
Abdullah <abdullahmz4640@gmail.com>
2026-01-13 23:14:24 +0500

    calculation logic

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let display = document.getElementById("display");
 
 let button = document.querySelectorAll(".btn");
 
+let result=0;
+
 for (let i = 0; i < button.length; i++) {
     let btn = button[i];
 
@@ -10,52 +12,67 @@ for (let i = 0; i < button.length; i++) {
         console.log (value);
         display.value += value;
 
-        if(value === "c"){
+        if(value === "="){
+            let op =  display.value.slice(-1);
+
+            display.value = display.value.slice(0, -1);
+
+            result = eval(display.value);
+            console.log(result);
+            display.value = "";
+            display.value += result;
+        }
+        else if(value === "c"){
             display.value= "";
         }
-        calculation();
 
     })
     
 }
 
 
-const calculation = () => {
+// const calculation = () => {
 
-    let arr = display.value.split('');
-    console.log (arr);
+//     let arr = display.value.split('');
+//     console.log (arr);
 
-    let currentValue= "";
-    let initailValue = [];
-    let finalValue =[];
-    let opretor = [];
+//     let currentValue= "";
+//     let initailValue = [];
+//     let finalValue =[];
+//     let opretor = [];
 
-    for(let i=0; i<=arr.length-1; i++){
+//     for(let i=0; i<=arr.length-1; i++){
         
-        let num = arr[i];
+//         let num = arr[i];
 
-        if(num >= '0' && num <= '9'){
-            currentValue += num ;
-           // console.log (currentValue);
+//         if(num >= '0' && num <= '9'){
+//             currentValue += num ;
+//            // console.log (currentValue);
 
-        } else if(num ==='+' || num ==='-' || num ==='*' || num ==='/'){
+//         } else if(num ==='+' || num ==='-' || num ==='*' || num ==='/'){
            
-             initailValue.push(Number(currentValue));
-             console.log (currentValue);
-             opretor.push(num);
-             currentValue = "";
+//              initailValue.push(Number(currentValue));
+//              console.log (currentValue);
+//              opretor.push(num);
+//              currentValue = "";
              
-        }
+//         }
 
-           console.log(opretor);
-           console.log(initailValue);
+//            console.log(opretor);
+//            console.log(initailValue);
 
-        }
+//         }
 
-        if(currentValue!== ""){
-            finalValue.push(Number(currentValue));
+//          if(currentValue!== ""){
+//             finalValue.push(Number(currentValue));
 
-            console.log(finalValue);
-    } 
+//             console.log(finalValue);
+//             console.log (currentValue);
+
+//          } 
+
+//          if(opretor="+"){
+//             initailValue + finalValue;
+//          }
     
-}
+// }

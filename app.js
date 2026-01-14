c60f0654c722b4e32a9708548c02e3d0d4588f1d
Abdullah <abdullahmz4640@gmail.com>
2026-01-15 04:01:10 +0500

    complete calculator

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 let display = document.getElementById("display");
-
 let button = document.querySelectorAll(".btn");
-
-let result=0;
+let result = 0;
 
 for (let i = 0; i < button.length; i++) {
     let btn = button[i];
 
     btn.addEventListener("click", () => {
         let value = btn.textContent;
-        console.log (value);
+        // console.log(value);
         display.value += value;
 
-        if(value === "="){
-            let op =  display.value.slice(-1);
+        if (value === "=") {
+            let opretor = ["+", "-", "*", "/"];
+
+            if (opretor.includes(display.value.at(0))) {
+                alert("Cannot start with operator!");
+                return;
+            }
+
+            let opt = display.value.at(-2);
+            // console.log(opt);
+
+            if (opretor.includes(opt)) {
+                alert("Invalid input");
+                display.value = display.value.slice(0, -1);
+                return;
+            }
 
             display.value = display.value.slice(0, -1);
 
             result = eval(display.value);
             console.log(result);
+
             display.value = "";
             display.value = result;
         }
 
-         if(value === "c"){
-            display.value= "";
-         }
-
-    })
-    
+        if (value === "c") {
+            display.value = "";
+        }
+    });
 }
 
 
